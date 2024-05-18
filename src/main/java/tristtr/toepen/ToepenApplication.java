93a2677dc21b90ed1c086bfa6779bf61271e8788
Tristan Rietveldt <t.rietveldt@student.tudelft.nl>
2024-05-18 16:00:43 +0200

    Initial commit

---
+++ src/main/java/tristtr/toepen/ToepenApplication.java
@@ -0,0 +1,13 @@
+package tristtr.toepen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToepenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToepenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tristtr/toepen/ToepenApplicationTests.java
@@ -0,0 +1,13 @@
+package tristtr.toepen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToepenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
