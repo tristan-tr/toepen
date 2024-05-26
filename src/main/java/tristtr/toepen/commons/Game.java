48cf809980bc8e36294c33ac5db5240b32508662
Tristan Rietveldt <t.rietveldt@student.tudelft.nl>
2024-05-26 14:59:22 +0200

    changed players from set to list

--- src/main/java/tristtr/toepen/commons/Game.java
+++ src/main/java/tristtr/toepen/commons/Game.java
@@ -2,20 +2,18 @@ package tristtr.toepen.commons;
 
 import lombok.Data;
 
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
 @Data
 public class Game {
     String name;
     List<Round> rounds;
-    Set<Player> players;
+    List<Player> players;
 
     public Game(String name) {
         this.name = name;
         this.rounds = new LinkedList<>();
-        this.players = new HashSet<>();
+        this.players = new LinkedList<>();
     }
 }
